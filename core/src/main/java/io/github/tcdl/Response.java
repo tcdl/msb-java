b786d72d44917c4266c5e18c7c120f615a852d86
rdro <roman.drozdov@thomascookonline.com>
2015-05-21 19:52:05 +0300

    default error handling in case of error in a microservice

--- core/src/main/java/io/github/tcdl/Response.java
+++ core/src/main/java/io/github/tcdl/Response.java
@@ -7,15 +7,6 @@ import java.util.Map;
  * Created by rdro on 4/29/2015.
  */
 public class Response {
-    private Map<?, ?> body = new HashMap<>();
-
-    public void setBody(Map<?, ?> body) {
-        this.body = body;
-    }
-
-    public Map<?, ?> getBody() {
-        return this.body;
-    }
 
     private Responder responder;
 
