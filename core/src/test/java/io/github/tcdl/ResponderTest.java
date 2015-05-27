6d7586889a2db2bcb4448da18191b68bc2047954
anstr <andrii.strashko@thomascookonline.com>
2015-05-27 17:34:04 +0300

    -Add new tests

--- core/src/test/java/io/github/tcdl/ResponderTest.java
+++ core/src/test/java/io/github/tcdl/ResponderTest.java
@@ -7,7 +7,6 @@ import io.github.tcdl.support.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Matchers.anyString;
@@ -21,13 +20,29 @@ public class ResponderTest {
     private MsbMessageOptions config;
     private static final String TOPIC = "test:responder";
 
-    @Mock
-    private MsbContext msbContext;
+    private ChannelManager mockChannelManager;
+    private Producer mockProducer;
+
+    private Payload payload;
+    private Message originalMessage;
+    private Responder responder;
 
     @Before
     public void setUp() {
-        this.config = TestUtils.createSimpleConfig();
-        this.msbContext = TestUtils.createSimpleMsbContext();
+        config = TestUtils.createSimpleConfig();
+
+        MsbContext msbContext = TestUtils.createSimpleMsbContext();
+        MsbContext msbContextSpy = spy(msbContext);
+        mockChannelManager = mock(ChannelManager.class);
+        mockProducer = mock(Producer.class);
+
+        payload = new Payload.PayloadBuilder().build();
+        originalMessage = TestUtils.createMsbRequestMessageWithPayloadAndTopicTo(TOPIC);
+
+        when(msbContextSpy.getChannelManager()).thenReturn(mockChannelManager);
+        when(mockChannelManager.findOrCreateProducer(anyString())).thenReturn(mockProducer);
+
+        responder = new Responder(config, originalMessage, msbContextSpy);
     }
 
     @Test
@@ -39,22 +54,28 @@ public class ResponderTest {
 
     @Test
     public void testProducerWasCreatedForProperTopic() {
-        ChannelManager mockChannelManager = mock(ChannelManager.class);
-        MsbContext msbContextSpy = spy(msbContext);
-        Producer producer = mock(Producer.class);
-
-        when(msbContextSpy.getChannelManager()).thenReturn(mockChannelManager);
-        when(mockChannelManager.findOrCreateProducer(anyString())).thenReturn(producer);
-
-        Payload payload = new Payload.PayloadBuilder().build();
-        Message originalMessage = TestUtils.createMsbRequestMessageWithPayloadAndTopicTo(TOPIC);
-        Responder responder = new Responder(config, originalMessage, msbContextSpy);
-
         ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
         responder.send(payload, null);
 
         verify(mockChannelManager).findOrCreateProducer(argument.capture());
 
         assertEquals(originalMessage.getTopics().getResponse(), argument.getValue());
+    }
+
+    @Test
+    public void testProducerPublishMethodInvoked() {
+        responder.send(payload, null);
+
+        verify(mockProducer, times(1)).publish(anyObject(), anyObject());
+    }
+
+    @Test
+    public void testProducerPublishUseCorrectPayload() {
+        ArgumentCaptor<Message> argument = ArgumentCaptor.forClass(Message.class);
+        responder.send(payload, null);
+
+        verify(mockProducer).publish(argument.capture(), anyObject());
+
+        assertEquals(payload, argument.getValue().getPayload());
     }
 }
