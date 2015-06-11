7658d37b982ca7e08c40c29b8dd666357ff419f2
vso-tc <volodymyr.sobotovych@thomascookonline.com>
2015-06-11 15:19:04 +0300

    Added test for AmqpProducerAdapterTest

---
+++ amqp/src/test/java/io/github/tcdl/adapters/amqp/AmqpProducerAdapterTest.java
@@ -0,0 +1,50 @@
+package io.github.tcdl.adapters.amqp;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+import io.github.tcdl.exception.ChannelException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AmqpProducerAdapterTest {
+    private Channel mockChannel;
+    private AmqpConnectionManager mockAmqpConnectionManager;
+
+    @Before
+    public void setUp() throws IOException {
+        Connection mockConnection = mock(Connection.class);
+        mockChannel = mock(Channel.class);
+        mockAmqpConnectionManager = mock(AmqpConnectionManager.class);
+
+        when(mockAmqpConnectionManager.obtainConnection()).thenReturn(mockConnection);
+        when(mockConnection.createChannel()).thenReturn(mockChannel);
+    }
+
+    @Test
+    public void testExchangeCreated() throws IOException {
+        String topicName = "myTopic";
+
+        new AmqpProducerAdapter(topicName, mockAmqpConnectionManager);
+
+        verify(mockChannel).exchangeDeclare(topicName, "fanout", false, true, null);
+    }
+
+    @Test
+    public void testPublish() throws ChannelException, IOException {
+        String topicName = "myTopic";
+        String message = "message";
+        AmqpProducerAdapter producerAdapter = new AmqpProducerAdapter(topicName, mockAmqpConnectionManager);
+
+        producerAdapter.publish(message);
+
+        verify(mockChannel).basicPublish(topicName, "" /* routing key */, MessageProperties.PERSISTENT_BASIC, message.getBytes());
+    }
+
+}
