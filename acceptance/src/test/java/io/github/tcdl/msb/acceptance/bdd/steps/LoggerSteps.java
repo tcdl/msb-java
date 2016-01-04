89885c6090ba6a5f882fee0639d5d16d390707d5
rkatsyuryna <ruslan.katsyuryna@thomascookonline.com>
2016-01-04 13:29:34 +0200

    Use of logback in microservices

--- acceptance/src/test/java/io/github/tcdl/msb/acceptance/bdd/steps/LoggerSteps.java
+++ acceptance/src/test/java/io/github/tcdl/msb/acceptance/bdd/steps/LoggerSteps.java
@@ -6,14 +6,14 @@ import org.jbehave.core.annotations.Then;
 import org.junit.Assert;
 
 public class LoggerSteps {
-    @Given("logger scanner reset")
-    public void start() {
-        ListAppender listAppender = ListAppender.getInstance();
-        listAppender.reset();
-    }
 
     @Then("log contains '$substring'")
     public void logContains(String substring) throws Exception {
         Assert.assertNotNull("String not found '" + substring + "'", ListAppender.getInstance().findLine(substring, 5, 500));
+    }
+
+    @Given("clear log")
+    public void clearLog() throws Exception {
+        ListAppender.getInstance().reset();
     }
 }
