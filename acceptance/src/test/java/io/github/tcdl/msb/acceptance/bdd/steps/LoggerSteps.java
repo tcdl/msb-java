5832fb38aa89f4f3669348616b30f76784cfb45f
anha1 <anton.haidai@thomascookonline.com>
2016-01-13 11:30:35 +0200

    WEB-17254 logback migration

--- acceptance/src/test/java/io/github/tcdl/msb/acceptance/bdd/steps/LoggerSteps.java
+++ acceptance/src/test/java/io/github/tcdl/msb/acceptance/bdd/steps/LoggerSteps.java
@@ -1,6 +1,7 @@
 package io.github.tcdl.msb.acceptance.bdd.steps;
 
-import io.github.tcdl.msb.acceptance.bdd.util.ListAppender;
+
+import io.github.tcdl.msb.acceptance.bdd.util.TestOutputStreamAppender;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.junit.Assert;
@@ -9,11 +10,11 @@ public class LoggerSteps {
 
     @Then("log contains '$substring'")
     public void logContains(String substring) throws Exception {
-        Assert.assertNotNull("String not found '" + substring + "'", ListAppender.getInstance().findLine(substring, 5, 500));
+        Assert.assertNotNull("String not found '" + substring + "'", TestOutputStreamAppender.isPresent(substring, 5, 500));
     }
 
     @Given("clear log")
     public void clearLog() throws Exception {
-        ListAppender.getInstance().reset();
+        TestOutputStreamAppender.reset();
     }
 }
