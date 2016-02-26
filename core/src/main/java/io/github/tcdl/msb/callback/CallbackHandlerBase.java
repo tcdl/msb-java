5b061f55efc20cee1cf3aac332533e72b14bb4d3
anha1 <anton.haidai@thomascookonline.com>
2016-02-26 09:59:18 +0200

    MsbContext shutdown callbacks support

--- core/src/main/java/io/github/tcdl/msb/callback/CallbackHandlerBase.java
+++ core/src/main/java/io/github/tcdl/msb/callback/CallbackHandlerBase.java
@@ -3,14 +3,14 @@ package io.github.tcdl.msb.callback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class CallbackHandlerBase implements CallbackHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(CallbackHandlerBase.class);
 
-    protected final Set<Runnable> callbacks = new HashSet<>();
+    protected final List<Runnable> callbacks = new ArrayList<>();
 
     public void runCallbacks() {
         for(Runnable callback: callbacks) {
